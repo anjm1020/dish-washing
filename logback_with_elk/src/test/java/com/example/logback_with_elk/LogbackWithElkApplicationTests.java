e75bb1af4c8a46d69d6e06c2c418d37c7f14aabb
jaemin <anjm1020@gmail.com>
2024-04-24 18:47:46 +0900

    feat: project init

---
+++ logback_with_elk/src/test/java/com/example/logback_with_elk/LogbackWithElkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logback_with_elk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackWithElkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
